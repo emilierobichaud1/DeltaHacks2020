f4c5f875223ab65e13c3df0fe19f7b6813b58c64
Emilie <emilie.robichaud@mail.utoronto.ca>
2020-01-25 23:13:15 -0500

    login screen

--- app/src/main/java/app/hackathon/com/deltahacks2020/HomeActivity.java
+++ app/src/main/java/app/hackathon/com/deltahacks2020/HomeActivity.java
@@ -1,36 +1,41 @@
 package app.hackathon.com.deltahacks2020;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.Manifest;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.FirebaseApp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private Button signUpButton;
+    private Button logInButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        signUpButton = (Button) findViewById(R.id.Sign_Up_Button);
+        logInButton = (Button) findViewById(R.id.Login_Button);
         FirebaseApp.initializeApp(this);
-    }
 
-    public void logIn(View view){
-        //go to log in page when log in button pressed
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_signup);
 
-    }
+            }
+        });
 
-    public void signUp(View view){
-        //go to sign up page when sign up button pressed
-        Intent intent = new Intent(this, SignupActivity.class);
-        startActivity(intent);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_login);
 
+            }
+        });
     }
 }
